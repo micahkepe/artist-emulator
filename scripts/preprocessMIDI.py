2186393f21102690a4a522f2cc4dbd4720e61197
MicahKepe <mik3@rice.edu>
2023-07-08 18:39:26 -0600

    More logging to preprocess script

--- scripts/preprocessMIDI.py
+++ scripts/preprocessMIDI.py
@@ -22,29 +22,37 @@ durations = []
 intensities = []
 
 # Iterate through all the MIDI files in the input directory
+logging.info(f"Preprocessing MIDI files from: {input_directory}")
 for filename in os.listdir(input_directory):
     if filename.endswith(".mid"):
         midi_path = os.path.join(input_directory, filename)
         
-        # Parsing the MIDI file
-        piece = converter.parse(midi_path)
+       # Parsing the MIDI file
+        try:
+            piece = converter.parse(midi_path)
+            logging.info(f"Parsing {filename}...")
 
-        # Extracting notes and attributes
-        for element in piece.flat:
-            if isinstance(element, note.Note):
-                notes.append(str(element.pitch))
-                durations.append(element.duration.quarterLength)
-                intensities.append(element.volume.velocity)
-            elif isinstance(element, chord.Chord):
-                notes.append('.'.join(str(n) for n in element.normalOrder))
-                durations.append(element.duration.quarterLength)
-                intensities.append(element.volume.velocity)
+            # Extracting notes and attributes
+            for element in piece.flat:
+                if isinstance(element, note.Note):
+                    notes.append(str(element.pitch))
+                    durations.append(element.duration.quarterLength)
+                    intensities.append(element.volume.velocity)
+                elif isinstance(element, chord.Chord):
+                    notes.append('.'.join(str(n) for n in element.normalOrder))
+                    durations.append(element.duration.quarterLength)
+                    intensities.append(element.volume.velocity)
+        except Exception as e:
+            logging.error(f"Error occurred while parsing {filename}: {str(e)}")
+            continue
 
 # Integer encoding for notes
+logging.info("Performing integer encoding...")
 note_encoder = LabelEncoder()
 notes_encoded = note_encoder.fit_transform(notes)
 
 # Normalization of durations and intensities
+logging.info("Performing normalization...")
 scaler = MinMaxScaler()
 durations_scaled = scaler.fit_transform(np.array(durations).reshape(-1, 1))
 intensities_scaled = scaler.fit_transform(np.array(intensities).reshape(-1, 1))
@@ -53,15 +61,19 @@ intensities_scaled = scaler.fit_transform(np.array(intensities).reshape(-1, 1))
 inputs = []
 outputs = []
 
+logging.info("Generating sequences of input and output data...")
 for i in range(len(notes_encoded) - sequence_length):
     inputs.append(notes_encoded[i:i + sequence_length])
     outputs.append(notes_encoded[i + sequence_length])
 
 # Split the data into training and testing sets
+logging.info("Splitting data into training and testing sets...")
 inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(inputs, outputs, test_size=test_split, random_state=42)
 
 # Save the preprocessed data
-np.savez(output_directory, inputs_train=inputs_train, outputs_train=outputs_train,
-         inputs_test=inputs_test, outputs_test=outputs_test)
-
-logging.info(f"Preprocessed data saved to: {output_directory}")
+try:
+    np.savez(output_directory, inputs_train=inputs_train, outputs_train=outputs_train,
+             inputs_test=inputs_test, outputs_test=outputs_test)
+    logging.info(f"Preprocessed data saved to: {output_directory}")
+except Exception as e:
+    logging.error(f"Error occurred while saving preprocessed data: {str(e)}")
